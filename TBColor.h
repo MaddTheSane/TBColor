ff7acb491ddb0a5ca483db4beb28e2206a9574c5
Zoreslav Khimich <zoreslav.khimich@gmail.com>
2012-07-21 13:20:42 +0700

    somewhat better examples

--- TBColor.h
+++ TBColor.h
@@ -11,7 +11,7 @@
 @interface TBColor : NSObject
 
 @property(nonatomic, readonly) CGColorRef CGColor;
-- (CGColorRef)ref;
+- (CGColorRef)ref; // shortcut for CGColor property
 
 - (id)initWithGenericGray:(CGFloat)gray alpha:(CGFloat)alpha;
 - (id)initWithGenericRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
@@ -19,8 +19,8 @@
 + (TBColor*)R:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue A:(CGFloat)alpha;
 + (TBColor*)R:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue;
 
-+ (TBColor*)fromARGB32:(uint32_t)argb32;    // e.g. CGColorRef transluentBlue = [TBColor fromARGB32:0x7F00FF00].CGColor;
-+ (TBColor*)fromRGB24:(uint32_t)rgb24;      // e.g. CGColorRef yellow = [TBColor fromRGB24:0xFFFF00].CGColor;
++ (TBColor*)fromARGB32:(uint32_t)argb32;    // e.g. TBColor *transluentBlue = [TBColor fromARGB32:0x7F00FF00];
++ (TBColor*)fromRGB24:(uint32_t)rgb24;      // e.g. TBColor *yellow = [TBColor fromRGB24:0xFFFF00];
 
 + (TBColor*)gray:(CGFloat)gray alpha:(CGFloat)alpha;
 + (TBColor*)gray:(CGFloat)gray;
